726fc72cb852552c275da21bcf823ecb74626b45
abhishektiwari003 <abhishektiwari@gofynd.com>
2026-04-15 16:43:27 +0530

    disconnected

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const PORT = 8080;
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(
-      "Monday updated the permission or install it again april testing 2026",
-    );
+    res.end("No permissions changes only disconnected");
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not Found");
